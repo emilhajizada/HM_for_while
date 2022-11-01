d31128df41833e30d2fffdd1eaef2a4689952718
Emil Hajizada <116425638+emilhajizada@users.noreply.github.com>
2022-11-01 21:48:09 +0400

    Added

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+int hasil = 1;
+for (int i = 3; i > 1; i--)
+{
+    hasil*=i;
+}
+Console.WriteLine(hasil);
+
+int eded = 12;
+bool isExist = false;
+for (int i = 2; i < eded; i++)
+{
+    if (eded % i == 0)
+    {
+        Console.WriteLine("eded murekkeb" + eded);
+        isExist = true;
+        break;
+    }
+}
+if (!isExist)
+{
+    Console.WriteLine( "sade" + eded);
+}
+
+
+
+int reqem = 134;
+int qaliq = 0;
+int mertebe = 0;
+while(reqem > 0)
+{
+    mertebe++;
+    qaliq = reqem %10;
+    reqem = (reqem - qaliq) /10;
+}
+
+Console.WriteLine(mertebe);
